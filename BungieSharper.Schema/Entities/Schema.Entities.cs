232faa1e874b197a907fefb0af1a7c5df251aed7
ashakoor <60638768+ashakoor@users.noreply.github.com>
2021-02-12 10:28:10 -0500

    forgot to remove the old stuff before merging, oops

--- BungieSharper.Schema/Entities/Schema.Entities.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace BungieSharper.Schema.Entities
-{
-    public class EntityActionResult
-    {
-        public long entityId { get; set; }
-
-        public Exceptions.PlatformErrorCodes result { get; set; }
-    }
-}
--- BungieSharper.Schema/Tags/Schema.Tags.Models.Contracts.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace BungieSharper.Schema.Tags.Models.Contracts
-{
-    public class TagResponse
-    {
-        public string tagText { get; set; }
-
-        public Ignores.IgnoreResponse ignoreStatus { get; set; }
-    }
-}
